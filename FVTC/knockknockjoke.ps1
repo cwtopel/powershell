e72310611fbe50a9e9c83098517f52d5308e71c3
Christopher W. Topel <ctopel@gmail.com>
2014-11-06 00:42:52 -0600

    Knock Knock Joke
    Project #1 from Microsoft Windows Powershell Programming for the Absolute Beginner.

---
+++ FVTC/knockknockjoke.ps1
@@ -0,0 +1,56 @@
+clear
+
+$userReply = ""
+
+while ($userReply -ne "Who is there?"){
+    $userReply = read-host "Knock Knock!"
+}
+
+clear
+while ($userReply -ne "Orange who?"){
+    $userReply = read-host "Orange."
+}
+
+clear
+Write-Output "Orange you glad you created this PowerShell script?"
+
+Start-Sleep -Seconds 5
+
+clear
+while ($userReply -ne "Who is there?"){
+    $userReply = read-host "Knock Knock!"
+}
+
+clear
+while ($userReply -ne "Orange who?"){
+    $userReply = read-host "Orange."
+}
+
+clear
+Write-Output "Oranges are oranges but this is PowerShell scripting!"
+
+Start-Sleep -Seconds 5
+
+clear
+
+while ($userReply -ne "Who is there?"){ 
+   $userReply = read-host "Knock Knock!"
+}
+
+clear
+while ($userReply -ne "Banana who?"){
+   $userReply = read-host "Banana."
+}
+
+clear
+Write-Output "Orange you glad I didn't say orange?"
+
+Start-Sleep -Seconds 5
+
+clear
+Write-Output "The Knock Knock Joke"
+Write-Output ""
+Write-Output "2014 Chris Topel"
+
+Start-Sleep -Seconds 3
+clear
