c17e2d3e723c9bad0018bbaa6c4cfff2049edd5a
Christopher W. Topel <cwtopel@users.noreply.github.com>
2014-11-07 04:47:18 -0600

    Update lockedout.ps1

--- Active-Directory/lockedout.ps1
+++ Active-Directory/lockedout.ps1
@@ -5,19 +5,16 @@ $AccountLockOutEventTime = $AccountLockOutEvent.TimeGenerated
 $AccountLockOutEventMessage = $AccountLockOutEvent.Message
 $Subject = "Account Locked Out: $LockedAccount" 
 $Body = "$LockedAccount was locked out on 
-$AccountLockOutEventTime
-
-`n`nEvent Details:
-`n`n$AccountLockOutEventMessage
-
-###########################################################################
+$AccountLockOutEventTime.`n`nEvent Details:`n`n$AccountLockOutEventMessage"
+           ###########################################################################
 # Please Configure the following variables....
 $smtpServer = "smtp.office365.com"
 $From = $LockedAccount+"@domain.com"
 $To = "support@domain.com"
 $O365UserAcct = "user@domain.com"
 $O365UserAcctPwd = "P@ssw0rd"  
-###########################################################################
+  ########################################################################### 
+
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
 $SMTPClient.EnableSsl = $true 
 $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($O365UserAcct, $O365UserAcctPwd); 
